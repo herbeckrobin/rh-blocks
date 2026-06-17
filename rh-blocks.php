3956828f5a5470d99400ab041e1bfde4e507a8f1
Robin Herbeck <info@robinherbeck.com>
2026-06-17 09:58:42 +0200

    Core 2.3.0 (Suite-Erweiterung), Version 0.1.1

--- rh-blocks.php
+++ rh-blocks.php
@@ -5,7 +5,7 @@
  * Plugin URI:        https://github.com/herbeckrobin/rh-blocks
  * Update URI:        https://github.com/herbeckrobin/rh-blocks
  * Description:       Bibliothek wiederkehrender Custom Blocks. Start: Link-Block (klickbare Group/Card). Teil der rh-blueprint Kollektion.
- * Version:           0.1.0
+ * Version:           0.1.1
  * Requires at least: 6.5
  * Requires PHP:      8.1
  * Author:            Robin Herbeck
@@ -21,7 +21,7 @@ if (! defined('ABSPATH')) {
     exit;
 }
 
-define('RHBLOCKS_VERSION', '0.1.0');
+define('RHBLOCKS_VERSION', '0.1.1');
 define('RHBLOCKS_PLUGIN_FILE', __FILE__);
 define('RHBLOCKS_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('RHBLOCKS_PLUGIN_URL', plugin_dir_url(__FILE__));
